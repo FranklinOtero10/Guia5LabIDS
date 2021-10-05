a1aba50d1d380f6d26cfce78694306db465d1982
FranklinOtero10 <franklin.otero6@gmail.com>
2021-10-05 15:06:39 -0600

    F

---
+++ application/views/shared/footer.php
@@ -0,0 +1,6 @@
+<div class="container text-right">
+    <p class="d-inline">Derechos reservados - USO - 2021</p>
+</div>
+</body>
+
+</html>
---
+++ application/views/shared/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $title ?></title>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+</head>
+
+<body>
+  <script src="<?php echo base_url('js/bootstrap.bundle.min.js'); ?>"></script>
+  <script src="<?php echo base_url(); ?>js/jquery-3.4.1.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/ajax.js"></script>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">USO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= site_url('estudiantesController') ?>">Estudiantes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= site_url('carrerasController') ?>">Carreras</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= site_url('MateriasController') ?>">Materias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= site_url('profesoresController') ?>">Profesores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= site_url('GruposController') ?>">Grupos</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
